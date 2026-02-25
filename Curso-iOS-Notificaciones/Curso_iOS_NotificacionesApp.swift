2f704b0f71a84663476ac03a335b4c90ce90ec16
Eduardo <eduardo@microsoft.com>
2026-02-25 13:47:34 +0100

    Initial Commit

---
+++ Curso-iOS-Notificaciones/Curso_iOS_NotificacionesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Curso_iOS_NotificacionesApp.swift
+//  Curso-iOS-Notificaciones
+//
+//  Created by Equipo 2 on 25/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Curso_iOS_NotificacionesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
